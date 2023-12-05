6d1ba9218efcfec33ad5139c97b5708ca9024254
mdking17 <mdking@uark.edu>
2023-12-05 15:50:35 -0600

    Added chart to ViewViceActivity

---
+++ app/src/main/java/com/example/vicetracker/ViewViceActivity/GraphAxes.kt
@@ -0,0 +1,29 @@
+package com.example.vicetracker.ViewViceActivity
+
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.utils.Utils
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+import kotlin.math.roundToInt
+
+
+class XAxisValueFormatter() : ValueFormatter() {
+    private val formatter = SimpleDateFormat("MMM-DD")
+    override fun getAxisLabel(value: Float, axis: AxisBase): String {
+
+        var position = value.roundToInt()
+
+        // get the date for the given position
+        val currentDate = Date().time
+        val positionDate = currentDate - 1000*3600*24 * (7 - position)
+        return formatter.format(positionDate)
+    }
+}
+
+class YAxisValueFormatter : ValueFormatter() {
+    override fun getAxisLabel(value: Float, axis: AxisBase): String {
+        return value.toString() + "k"
+    }
+}
--- app/src/main/java/com/example/vicetracker/ViewViceActivity/ViewViceActivity.kt
+++ app/src/main/java/com/example/vicetracker/ViewViceActivity/ViewViceActivity.kt
@@ -6,6 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.vicetracker.NewViceActivity.EXTRA_ID
 import com.example.vicetracker.R
 import com.example.vicetracker.VicesApplication
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.Entry
+
 
 class ViewViceActivity : AppCompatActivity() {
     //This is our viewModel instance for the MainActivity class
@@ -13,15 +21,19 @@ class ViewViceActivity : AppCompatActivity() {
         ViewViceViewModelFactory((application as VicesApplication).repository, -1)
     }
 
+    lateinit var barChart: BarChart
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_vice)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        // get UI components
         val toolbar = getSupportActionBar()
         if (toolbar != null) {
             toolbar.setDisplayHomeAsUpEnabled(true)
         }
+        barChart = findViewById(R.id.chart)
 
         // Get ID from intent
         val id = intent.getIntExtra(EXTRA_ID,-1)
@@ -29,11 +41,30 @@ class ViewViceActivity : AppCompatActivity() {
         viewViceViewModel.curVice.observe(this) {
                 vice->vice?.let { if (toolbar != null) { toolbar.setTitle(vice.name) } }
         }
+
+        // format chart
+        val xAxis: XAxis = barChart.getXAxis()
+        val yAxis: YAxis = barChart.getAxisLeft()
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM)
+        xAxis.setValueFormatter(XAxisValueFormatter())
+
+        // put data on chart
+        // Create list of entry objects with data and X position
+        val data = listOf(1,2,3,4,5,6,7)
+        val dataEntries = mutableListOf<BarEntry>()
+        for (i in data.indices) {
+            dataEntries.add(BarEntry(i.toFloat(), data[i].toFloat()))
+        }
+
+        // give data lable and add to chart
+        val dataSet = BarDataSet(dataEntries, "Daily Engagement")
+        barChart.data = BarData(dataSet)
     }
 
-    // override action bar back button callback
+    // override action bar back button function
     override fun onSupportNavigateUp(): Boolean {
-        setResult(AppCompatActivity.RESULT_OK)
+        setResult(RESULT_OK)
         finish()
         return true
     }
